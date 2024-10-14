4d5b72465c13e7fa74e29aaef7f016b46f3756e2
chasov-artem <chasov90@gmail.com>
2024-10-14 13:43:54 +0300

    router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Products from "./pages/Products";
+import Cart from "./pages/Cart";
+import NotFound from "./pages/NotFound";
 
 function App() {
-  return <></>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,8 @@
+const NotFound = () => {
+  return (
+    <div>
+      <h2>NotFound</h2>
+    </div>
+  );
+};
+export default NotFound;
